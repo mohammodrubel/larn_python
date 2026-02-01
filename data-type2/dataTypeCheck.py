5f0d52fcdc764210d6d305ef0cdd3d318a8aa9b1
mohammodrubel <md.rubel007@hotmail.com>
2026-02-01 21:56:51 +0600

    check data type

---
+++ data-type2/dataTypeCheck.py
@@ -0,0 +1,40 @@
+
+a = 10
+b = 3.4
+c = 1+2j
+d = "Hello"
+e = ['apple','age','alice']
+f = (1,2,3)
+g = range(1,10,2)
+h = {"name":"rubel","age":20}
+i = {1,2,3}
+j = frozenset([1,2,3])
+k=True
+i = None
+
+
+check = type(a)
+check1 = type(b)
+check2 = type(c)
+check3 = type(d)
+check4 = type(e)
+check5 = type(f)
+check6 = type(g)
+check7 = type(h)
+check8 = type(i)
+check9 = type(j)
+check10 = type(k)
+check11 = type(i)
+
+print(check)
+print(check1)
+print(check2)
+print(check3)
+print(check4)
+print(check5)
+print(check6)
+print(check7)
+print(check8)
+print(check9)
+print(check10)
+print(check11)
