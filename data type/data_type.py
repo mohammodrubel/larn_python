b979fc4d6397d1fc42bc34cdd3cc1f630a21c6af
mohammodrubel <md.rubel007@hotmail.com>
2026-02-01 21:40:08 +0600

    update data type

---
+++ data type/data_type.py
@@ -0,0 +1,33 @@
+# data = 10 ইন্টেজার 
+
+
+# data2 = 22.22 ফ্লট 
+
+# data = "Hello world" স্ট্রিং 
+
+# complexNumber = 10 + 20j কম্পেলেক্স 
+
+# data3 = ['dhaka','rangpur',True, 10,10.25,'dhaka'] লিস্ট 
+
+# latter = ("rubel", "hasan", "rounok", True, False) টুপল 
+
+# isBangladeshi = True 
+
+# NoneData = None 
+
+# print(NoneData)
+
+person = {
+    "name":"Rubel",
+    "age":"28",
+    "location":"Bangladesh"
+}
+print(person)
+print(person["age"])
+print(person["location"])
+
+uniqueNumber = {1,2,3,2}
+print(uniqueNumber)
+
+myForegenlist = frozenset([1,2,3,4,5,6,1,2,3])
+print(myForegenlist)
