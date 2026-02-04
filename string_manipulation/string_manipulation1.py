c94b866166fc41e8cf8caa63b9d6d8f1e0e26808
mohammodrubel <md.rubel007@hotmail.com>
2026-02-04 15:01:09 +0600

    string manipulation

---
+++ string_manipulation/string_manipulation1.py
@@ -0,0 +1,27 @@
+
+# strings 
+# ''  ""  `  ''  ""  `
+
+name = 'Hello world'
+
+name1 = "Hello world"
+
+name3 = """ Hello world"""
+
+
+
+# single quotes string 
+info = 'he said,"hello world"'
+
+
+
+#duble quotes string 
+information = "he said,'Hello world'"
+
+
+#triple quotes string 
+my_string  = """
+he said , 'bangladesh' is a "green country". 
+and my name is md rbuel
+"""
+print( my_string )
