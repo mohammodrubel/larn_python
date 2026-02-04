be5c7e06b6568379fc237c220f31e75071078323
mohammodrubel <md.rubel007@hotmail.com>
2026-02-04 15:20:39 +0600

    string slice

---
+++ string_manipulation/string_manipulation_index.py
@@ -0,0 +1,19 @@
+
+text = "Hello world"
+
+print(text[2]) #possitive index starting "H"
+
+
+print(text[-1]) #magatove index starting "d"
+print(text[-2]) #magatove index starting "d"
+
+# string sliceing 
+
+print(text[0:5]) #hello
+print(text[6:11]) #world
+print(text[3:]) #lo world
+print(text[:5]) #hello 
+print(text[0::3]) #Hlwl 3 ta kore kata shuru hobe  
+
+
+
