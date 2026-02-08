2f64994920b0e73ada3cadc17258fe0a8b027f94
mohammodrubel <md.rubel007@hotmail.com>
2026-02-08 08:11:04 +0600

    caseconverter

---
+++ string_manipulation/string_operations.py
@@ -0,0 +1,26 @@
+
+name = "fardin tazbeed"
+
+repet= name * 3 
+
+
+
+# string concate
+
+first_name ="fardin"
+last_name="tazbeed"
+
+combine = "".join([first_name,last_name])
+formate_mathod = "{} {}".format(first_name,last_name)
+
+print(first_name + " " + last_name)
+print(combine)
+print(formate_mathod)
+
+# 6.4
+
+str1 = "Hello"
+str2 = "world"
+
+total_combine = "%s %s" %(str1,str2)
+print(total_combine)
