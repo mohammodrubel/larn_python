99dafe2bf52eb423c9ed2cb6dbd0e702924cd361
mohammodrubel <md.rubel007@hotmail.com>
2026-02-16 09:29:01 +0600

    update math

---
+++ NumberAndMath/Math.py
@@ -0,0 +1,33 @@
+import math
+# স্কয়ার রুট 
+print (math.sqrt(16)) 
+# পাওয়ার 
+print(math.pow(4,2))
+# ত্রিকোণমিতি 
+# সাইন মেথড  
+print(math.sin(math.radians(90)))
+print(math.cos(math.radians(90)))
+# python logarithm
+print(math.log(10))
+print(math.log10(10))
+print(math.log2(10))
+
+# fectorial 
+print(math.factorial(5))
+print(math.factorial(2))
+
+# absulute value 
+print(math.fabs(-7.5))
+
+# flor and ceiling 
+print(math.floor(3.7))
+print(math.ceil(3.8))
+print(math.pi)
+
+
+
+num1 = 12 
+num2 = 18 
+
+print(math.gcd(num1,num2))
+print(math.lcm(num1,num2))
