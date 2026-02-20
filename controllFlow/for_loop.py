52fd062fa9121e7f92d8d3d77c6491606bc77d6b
mohammodrubel <md.rubel007@hotmail.com>
2026-02-21 03:20:51 +0600

    for loop

---
+++ controllFlow/for_loop.py
@@ -0,0 +1,46 @@
+
+# frouts = ['apple','banana','orange']
+
+# print(frouts[0])
+# print(frouts[1])
+# print(frouts[2])
+
+# # now try to dynamic and use loop same work 
+
+# for each in frouts:
+#     print(each) 
+
+# test = "Hello world"
+# words = test.split()  
+# mainData = ",".join(words)
+# print(mainData)
+# print(words)
+# for item in test:
+#     print(item)
+# for item in words:
+#     print(item)
+
+
+# for item in range(100):
+#     print(item)
+
+
+# # যদি ডিকশনারির উপরে ফর লুপ চলাই তাহলে  এবং ডিকশনারি হলো জাভাস্ক্রিপ্ট এর অবজেক্ট 
+
+
+# exam_result = {"bangla":58,"english":88,"arabic":33}
+
+# print(exam_result)
+# for subject,markes in exam_result.items():
+#     print("subject"," : ", subject, " " , "markes"," : ", markes)
+
+
+
+# unique_number = {1,2,3,4,5,6,7,8,9}
+# for singleNumbner in unique_number:
+#     print(singleNumbner)
+
+for num in range(10):
+    if num == 3:
+        continue
+    print(num)
