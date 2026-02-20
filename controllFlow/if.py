568df9be252aec27e1161d16c6a18ce2f4a40755
mohammodrubel <md.rubel007@hotmail.com>
2026-02-21 02:27:57 +0600

    if else nested if else

---
+++ controllFlow/if.py
@@ -0,0 +1,40 @@
+
+# age = 5
+
+# if age>=18:
+#     print("you are a adult")
+# else : 
+#     print("you are child")
+    
+
+# marks = 51
+
+
+# if marks >= 80  and marks <=100 :
+#     print("you Got A+")
+# elif marks >=70 and marks <= 79:
+#     print("you got A")
+# elif marks >= 60 and marks <= 69:
+#     print("you Got A-") 
+# elif marks >= 50 and marks <=59:
+#     print("you Got B+")
+# elif marks >= 40 and marks <=49 :
+#     print("You Got C")
+# elif marks >= 33 and marks <=39 :
+#     print("you Got D")
+# elif marks >= 0 and marks <=32 :
+#     print("You Are Faill! Try again next time")
+
+
+
+age = 17
+permission = True
+
+if age >= 18:
+    print("you are able to join club")
+    if permission == True:
+        print("you can drink")
+    else:
+        print("you can't drink not permission")
+else:
+    print("you are not able to join club. try next year")
