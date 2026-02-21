7076185d6bcc0900a44c09ad3429f7220cc7f92b
mohammodrubel <md.rubel007@hotmail.com>
2026-02-21 17:53:46 +0600

    update

---
+++ controllFlow/and_or_not.py
@@ -0,0 +1,22 @@
+# age = 20 
+# has_permisstion = False 
+
+# if age>18 and has_permisstion == True:
+#     print("you are old enough")
+# else:
+#     print("you are not old enough")
+
+age = 20 
+has_permisstion = False 
+
+if age>18 or has_permisstion == True:
+    print("you are old enough")
+else:
+    print("you are not old enough")
+
+age = 2 
+
+if age>= 18 :
+    print("you are adult")
+else:
+    print("you are not adult")
