17272504d7f96b5c5ac41d8cd56b34a8b1ea8e2e
mohammodrubel <md.rubel007@hotmail.com>
2026-02-21 17:45:47 +0600

    for and while loop

---
+++ controllFlow/whileLoop.py
@@ -0,0 +1,62 @@
+
+# হোয়াইল লুপ চলার নিয়ম 
+
+
+
+# স্টার্টিং পয়েন্ট বলে দিতে হবে
+#  ইন্ড পয়েন্ট বলে দিতে হবে 
+# কতো করে লুপ টা ঘুরবে তা বলে দিতে হবে 
+
+# frouts = ["apple","cherry","banana","orange"]
+# index = 0
+# while index< len(frouts) :
+#     print(index)
+#     index = index + 1
+
+# froutes = ["apple","cherry","banana","orange"]
+# myList = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9]
+
+# index = 0
+
+# while index<len(myList):
+#     print (index)
+#     index = index+1 
+    
+# while index<len(froutes):
+#     print (index)
+#     index = index+1 
+
+# myString = "Hello world"
+# index = 0
+# while index<len(myString):
+#     print (myString[index])
+#     index=index + 1
+
+# index = 0
+# end = 10 
+
+# while index <= end :
+#     index = index + 1
+
+#     if index % 2 == 0:
+#         continue
+
+#     print(index)
+
+# exam_result = {"bangla":72,"english":62,"statictics":32}
+# index = 0
+# keys=list(exam_result.keys())
+
+# while index<len(keys):
+#     each_key = keys[index]
+#     output="subject: " + each_key+ Markes=" "
+
+my_set = {1,2,3,4,5,6,7,8,9}
+my_list = list(my_set)
+index = 0
+print(my_list)
+print(my_set)
+
+while index< len(my_list) :
+    print(my_list[index])
+    index = index + 1
